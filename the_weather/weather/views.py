12018d6124643a0b8e85e685a553bd6f7128d94e
MTomilov <mtomilov@awardwallet.com>
2018-07-26 22:44:57 +0500

    style

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -2,9 +2,13 @@ import requests
 from django.shortcuts import render
 from .models import City
 from .forms import CityForm
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=YOUR_API_KEY'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=898ffc72e88c5f89c3d3274f84335e20'
 
     if request.method == 'POST':
         form = CityForm(request.POST)
@@ -18,16 +22,19 @@ def index(request):
 
     for city in cities:
 
-        r = requests.get(url.format(city)).json()
+        data = requests.get(url.format(city)).json()
 
-        city_weather = {
-            'city' : city.name,
-            'temperature' : r['main']['temp'],
-            'description' : r['weather'][0]['description'],
-            'icon' : r['weather'][0]['icon'],
-        }
+        try:
+            city_weather = {
+                'city': city.name,
+                'temperature': data['main']['temp'],
+                'description': data['weather'][0]['description'],
+                'icon': data['weather'][0]['icon'],
+            }
+        except KeyError:
+            break
 
         weather_data.append(city_weather)
 
-    context = {'weather_data' : weather_data, 'form' : form}
+    context = {'weather_data': weather_data, 'form': form}
     return render(request, 'weather/weather.html', context)
